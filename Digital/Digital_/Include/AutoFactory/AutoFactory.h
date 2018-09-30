30c3abffa71dd0ca85f053113a472d346ee280a8
TDCRanila <thedigitalchan@gmail.com>
2018-09-30 14:38:16 +0200

    [] Changed File(s) | AutoFactory Files CleanUp #
    - Moved some code around to clean up the AutoFactory implementation.
    - Changed GetFactories() to return a pointer to the factory map instead of a reference to said map.

--- Digital/Digital_/Include/AutoFactory/AutoFactory.h
+++ Digital/Digital_/Include/AutoFactory/AutoFactory.h
@@ -5,17 +5,17 @@
 #include <string>
 
 namespace DFactory {
-	
-	class AutoFactoryInterface {
-	public:
-		AutoFactoryInterface() {/*EMPTY*/}
-		virtual ~AutoFactoryInterface() {/*EMPTY*/}
-	};
-	
+		
+	class AutoFactoryInterface;
+
 	typedef std::unordered_map<std::type_index, AutoFactoryInterface*> ClassFactoryMap;
 	typedef std::unordered_map<std::type_index, AutoFactoryInterface*>::iterator ClassFactoryMapIt;
 	
-	std::unordered_map<std::string, ClassFactoryMap>& GetFactories();
+	class AutoFactoryInterface {
+	public:
+		AutoFactoryInterface() {/*EMPTY*/ }
+		virtual ~AutoFactoryInterface() {/*EMPTY*/ }
+	};
 
 	template <class T>
 	class AutoFactory : public AutoFactoryInterface {
@@ -26,16 +26,36 @@ namespace DFactory {
 		virtual T* Create() = 0;
 	};
 
-	static ClassFactoryMap* GetFactoryList(std::string a_list_name) {
-		auto it_list = DFactory::GetFactories().find(a_list_name);
-		if (it_list != DFactory::GetFactories().end()) {
-			return &it_list->second;
-		}
-		return nullptr;
-	}
+	// TODO: // Function to get map with class factories.
+	std::unordered_map<std::string, ClassFactoryMap>* GetFactories();
+	
+	// TODO: // Function to get a map of class factories under a given list name identifier.
+	ClassFactoryMap* GetFactoryList(std::string a_list_name);
+
+	// TODO: // Function to construct a class from a factory under a given list name identifier.
+	template <class T>
+	T* ConstructFromFactory(std::string a_list_name);
+
+	// TODO: // Function to get get a pointer to a class factory of specified type under a given list name identifier.
+	template <class T>
+	AutoFactory<T>* GetFactoryOfType(std::string a_list_name);
+
+#define REGISTER_TYPE(fclass, list)									\
+class fclass##Factory : public DFactory::AutoFactory<fclass> {		\
+public:																\
+	fclass##Factory() {												\
+		(*DFactory::GetFactories())[list][typeid(fclass)] = this;	\
+	}																\
+	virtual fclass* Create() override {								\
+		return new fclass();										\
+	}																\
+};																	\
+static fclass##Factory global##fclass##FactoryObject;
+
+#pragma region Template Function Implementation
 
 	template <class T>
-	static T* ConstructFromFactory(std::string a_list_name) {
+	T* ConstructFromFactory(std::string a_list_name) {
 		// Find List
 		auto it_list = DFactory::GetFactories().find(a_list_name);
 		if (it_list != DFactory::GetFactories().end()) {
@@ -51,30 +71,20 @@ namespace DFactory {
 	}
 
 	template <class T>
-	static AutoFactory<T>* GetFactoryOfType(std::string a_list_name) {
+	AutoFactory<T>* GetFactoryOfType(std::string a_list_name) {
 		// Find List
 		auto it_list = DFactory::GetFactories().find(a_list_name);
 		if (it_list != DFactory::GetFactories().end()) {
 			// Find Factory
 			auto it_fac = it_list->second.find(typeid(T));
 			if (it_fac != it_list->second.end()) {
-			// TODO: DEBUG->DynamicCast | RELEASE->StaticCast
-			return dynamic_cast<AutoFactory<T>*>(it_fac->second);
+				// TODO: DEBUG->DynamicCast | RELEASE->StaticCast
+				return dynamic_cast<AutoFactory<T>*>(it_fac->second);
 			}
 		}
 		return nullptr;
 	}
 
-#define REGISTER_TYPE(fclass, list)								\
-class fclass##Factory : public DFactory::AutoFactory<fclass> {	\
-public:															\
-	fclass##Factory() {											\
-		DFactory::GetFactories()[list][typeid(fclass)] = this;	\
-	}															\
-	virtual fclass* Create() override {							\
-		return new fclass();									\
-	}															\
-};																\
-static fclass##Factory global##fclass##FactoryObject;
+#pragma endregion
 
 } // End of namespace ~ DFactory
--- Digital/Digital_/Source/AutoFactory/AutoFactory.cpp
+++ Digital/Digital_/Source/AutoFactory/AutoFactory.cpp
@@ -2,10 +2,18 @@
 
 namespace DFactory {
 
-	std::unordered_map<std::string, ClassFactoryMap>& GetFactories() {
+	std::unordered_map<std::string, ClassFactoryMap>* GetFactories() {
 		static std::unordered_map<std::string, ClassFactoryMap>* factories =
 			new std::unordered_map<std::string, ClassFactoryMap>();
-		return *factories;
+		return factories;
+	}
+
+	ClassFactoryMap* GetFactoryList(std::string a_list_name) {
+		auto it_list = DFactory::GetFactories()->find(a_list_name);
+		if (it_list != DFactory::GetFactories()->end()) {
+			return &it_list->second;
+		}
+		return nullptr;
 	}
 
 } // End of namespace ~ DFactory
