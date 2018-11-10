bea684fd8f938dcdc0229784005d5787f3673a22
TDCRanila <thedigitalchan@gmail.com>
2018-11-10 20:44:19 +0100

    [] Changed File(s) | Windows MinMax Undefs & Added Comments #

--- Digital/Digital_/Include/Defines/Defines.h
+++ Digital/Digital_/Include/Defines/Defines.h
@@ -2,22 +2,33 @@
 
 #include <iostream>
 
+// Debug Assert Defines
 #if defined(DEBUG64) || defined(PRERELEASE64)
-#define DASSERT(...) _ASSERT(__VA_ARGS__)
+	#include <assert.h>
+	#define DASSERT(...) assert(__VA_ARGS__)
 #elif defined(RELEASE64)
-#define DASSERT(...) { /*EMPTY*/ }
+	#define DASSERT(...) { /*EMPTY*/ }
 #endif
 
+// Directory Slashes Defines
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-#define DIR_SLASH_CHAR	'\\'
-#define DIR_SLASH		"\\"
+	#define DIR_SLASH_CHAR	'\\'
+	#define DIR_SLASH		"\\"
 #else
-#define DIR_SLASH_CHAR	'/'
-#define DIR_SLASH		"/"
+	#define DIR_SLASH_CHAR	'/'
+	#define DIR_SLASH		"/"
 #endif
 
+// Unused Variable Define
 #define UNUSED(variable) (void)(variable)
 
+// Windows Min & Max Undefs
+#if defined(min) && defined(max)
+	#undef min
+	#undef max
+#endif
+
+// Logging Defines
 #if defined(DEBUG64) || defined(PRERELEASE64)
 
 #include <chrono>
