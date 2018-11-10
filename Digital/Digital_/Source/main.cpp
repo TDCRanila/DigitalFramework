7c8abfe47b8ce0f9744d3b695162401a1f2c7891
TDCRanila <thedigitalchan@gmail.com>
2018-11-10 20:08:25 +0100

    [] Changed File(s) | main.cpp testbench cleanup #

--- Digital/Digital_/Source/main.cpp
+++ Digital/Digital_/Source/main.cpp
@@ -1,39 +1,5 @@
 #include <iostream>
 
-#include <ECS/Objects/ECSystem.h>
-//#include <ECS/ECSEntity.h>
-
-#include <AutoFactory/AutoFactory.h>
-
-#include <Defines/Defines.h>
-
-//class SisterSystem : public DECS::ECSystem::Registrar<SisterSystem> {
-//public:
-//	SisterSystem() {
-//
-//	}
-//
-//	~SisterSystem() {
-//
-//	}
-//};
-//
-//class KappaSystem : public DECS::ECSystem::Registrar<KappaSystem> {
-//public:
-//	KappaSystem() {
-//
-//	}
-//
-//	~KappaSystem() {
-//
-//	}
-//};
-
 int main(int /*argc*/, char** /*argv*/) {
-
-	auto factories = DECS::ECSystem::GetFactories();
-
-	auto blop1 = DECS::ECSystem::Construct("KappaSystem");
-	auto blop2 = DECS::ECSystem::Construct("SisterSystem");
 
 }
