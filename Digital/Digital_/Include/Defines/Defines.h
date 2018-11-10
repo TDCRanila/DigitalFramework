c8fc22a74139933db23c1dde1021773d57e6f8bb
TDCRanila <thedigitalchan@gmail.com>
2018-11-10 20:08:02 +0100

    [] Changed File(s) |Added  Project Logging, Assert, Unused, and DirSlash Defines #
    - Defines for Logging, Asserting, Specifying Unused variables, and DirSlash added. Define implementation changes based on project settings.

--- Digital/Digital_/Include/Defines/Defines.h
+++ Digital/Digital_/Include/Defines/Defines.h
@@ -1,7 +1,54 @@
 #pragma once
 
-namespace DDefines {
+#include <iostream>
 
-#define ComponentListName "Components"
+#if defined(DEBUG64) || defined(PRERELEASE64)
+#define DASSERT(...) _ASSERT(__VA_ARGS__)
+#elif defined(RELEASE64)
+#define DASSERT(...) { /*EMPTY*/ }
+#endif
 
-} // End of namespace ~ DDefines
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#define DIR_SLASH_CHAR	'\\'
+#define DIR_SLASH		"\\"
+#else
+#define DIR_SLASH_CHAR	'/'
+#define DIR_SLASH		"/"
+#endif
+
+#define UNUSED(variable) (void)(variable)
+
+#if defined(DEBUG64) || defined(PRERELEASE64)
+
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+	namespace {
+		static inline std::string GetTime() {
+			std::time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+			std::ostringstream oss;
+			oss << std::put_time(localtime(&t), "%F %T");
+			return oss.str();
+
+		}
+
+		const std::string g_info_label	= "INFO ";
+		const std::string g_debug_label	= "DEBUG";
+		const std::string g_error_label	= "ERROR";
+
+	} // End of namespace
+
+	#define __FILENAME__ (strrchr(__FILE__, DIR_SLASH_CHAR) ? strrchr(__FILE__, DIR_SLASH_CHAR) + 1 : __FILE__)
+
+	#define INFOLOG(message)  std::cout << g_info_label		<< " | " << __FILENAME__	<< "(" << __LINE__ << "): "  << #message << std::endl;
+	#define DEBUGLOG(message) std::cout << g_debug_label	<< " | " << __FILENAME__	<< "(" << __LINE__ << "): "  << #message << std::endl;
+	#define ERRORLOG(message) std::cout << g_error_label	<< " | " << __FILENAME__	<< "(" << __LINE__ << "): "  << #message << std::endl;
+
+#elif defined(RELEASE64)
+	#define INFOLOG(message)  { /*EMPTY*/ }
+	#define DEBUGLOG(message) { /*EMPTY*/ }
+	#define ERRORLOG(message) { /*EMPTY*/ }
+#endif
