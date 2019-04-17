04ad5c7037e005f21b80f03999d10ec3f433c74a
TDCRanila <thedigitalchan@gmail.com>
2019-04-17 21:38:02 +0200

    [] Added File(s) | Added Simple Templated ComponentHandle class #

---
+++ Digital/Digital_/Include/ECS/Objects/ECSComponentHandle.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <ECS/Objects/ECSComponent.h>
+
+namespace DECS
+{
+	template <class T>
+	class ECSComponentHandle
+	{
+	public:
+		ECSComponentHandle();
+		ECSComponentHandle(T* a_component);
+		~ECSComponentHandle();
+
+		bool IsComponentValid() const;
+
+		T* operator->() const;
+
+	private:
+		T* component_ptr;
+
+	};
+
+#pragma region Template Function Implementation
+
+	template <class T>
+	ECSComponentHandle<T>::ECSComponentHandle() :
+		component_ptr(nullptr)
+	{
+		/*EMPTY*/
+	}
+
+	template <class T>
+	ECSComponentHandle<T>::ECSComponentHandle(T* a_component) :
+		component_ptr(a_component)
+	{
+		/*EMPTY*/
+	}
+
+	template <class T>
+	ECSComponentHandle<T>::~ECSComponentHandle() { /*EMPTY*/ }
+
+	template <class T>
+	bool ECSComponentHandle<T>::IsComponentValid() const
+	{
+		return component_ptr;
+	}
+
+	template <class T>
+	T* ECSComponentHandle<T>::operator->() const
+	{
+		return component_ptr;
+	}
+
+#pragma endregion
+
+} // End of namespace ~ DECS
---
+++ Digital/Digital_/Source/ECS/Objects/ECSComponentHandle.cpp
@@ -0,0 +1,1 @@
+#include <ECS/Objects/ECSComponentHandle.h>
