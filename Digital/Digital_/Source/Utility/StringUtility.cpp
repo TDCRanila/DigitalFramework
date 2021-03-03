1865800b6ff617e62f2f72f0323b4416dc7e6648
TDRChan <thedigitalchan@gmail.com>
2021-03-03 16:44:21 +0100

    [Project, Utilities] Moving/Adding some extra string utilities. #

--- Digital/Digital_/Source/Utility/StringUtility.cpp
+++ Digital/Digital_/Source/Utility/StringUtility.cpp
@@ -17,4 +17,35 @@ namespace DUtility
 		}
 	}
 
+	void FindAndRemoveChar(std::string& a_original_string, char a_char_to_remove)
+	{
+		a_original_string.erase(std::remove(a_original_string.begin(), a_original_string.end(), a_char_to_remove));
+	}
+
+	bool FindAndReplaceChar(std::string& a_original_string, char a_char_to_remove, char a_char_to_replace)
+	{
+		bool has_replaced = false;
+		for (char& c : a_original_string)
+		{
+			if (c == a_char_to_remove)
+			{
+				has_replaced = true;
+				c = a_char_to_replace;
+			}
+		}
+		return has_replaced;
+	}
+
+	bool ContainsChar(const std::string a_string, char a_chararcter)
+	{
+		return a_string.find(a_chararcter) != std::string::npos;
+	}
+
+	bool StringToBool(std::string a_string)
+	{
+		if		(a_string == "0" || a_string == "FALSE")	{ return false; }
+		else if (a_string == "1" || a_string == "TRUE")		{ return true; }
+		else												{ return false; }
+	}
+
 } // End of namespace ~ DUtility
