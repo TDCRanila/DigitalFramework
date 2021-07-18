05f4d0543aae1ba63d6c8790f901226b48358b74
TDRChan <thedigitalchan@gmail.com>
2021-07-18 19:00:18 +0200

    [Project, Core, Logging] Temporarily disabling operators for custom type logging as it was causing issues. #

--- Digital/Digital_/Source/CoreSystems/Logging/CustomTypes.h
+++ Digital/Digital_/Source/CoreSystems/Logging/CustomTypes.h
@@ -2,14 +2,24 @@
 
 #include <spdlog/fmt/ostr.h>
 
+#include <CoreSystems/DObject.h>
+
 #include <Defines/IDDefines.h>
 
-#pragma region IDs
+#pragma region CoreTypes
 
-    template<typename OStream>
-    OStream& operator<<(OStream& os, const DCore::DUID& c)
-    {
-        return os << "DUID-" << c.operator std::string();
-    }
+    // TODO Need to look into this again regarding operators for custom logging types.
+
+    //template<typename OStream>
+    //OStream& operator<<(OStream& os, const DCore::DUID& c)
+    //{
+    //    return os << "DUID-" << c.operator std::string();
+    //}
+
+    //template<typename OStream>
+    //OStream& operator<<(OStream& os, const DCore::DObject& c)
+    //{
+    //    return os << "DObject-" << c.operator std::string();
+    //}
 
 #pragma endregion
