c1515f29c3a0a424d1b485f5d28982dc4bfed4c1
TDRChan <thedigitalchan@gmail.com>
2021-10-04 16:24:48 +0200

    [Project, ECS] KeyLockSystem cleaned up. # - Replaced manual parameter pack unpacking with a simple function using constexpr. - Using concepts instead of std::enanble_ifs. - Fixed 'IsComponentBitTrue' function. - Code standard, Magic numbers, default constructors, consts...., etc.

--- Digital/Digital_/Source/Modules/ECS/Utility/ECSKeyLockSystem.h
+++ Digital/Digital_/Source/Modules/ECS/Utility/ECSKeyLockSystem.h
@@ -14,131 +14,146 @@
 
 namespace DECS 
 {
-	const int64 UNASSIGNED_COMPONENT_BIT = -1;
+	const int64 DFW_UNASSIGNED_COMPONENT_BIT	= -1;
+	const int8	DFW_MAX_REGISTERED_COMPONENTS	= 64;
+
+	// Concepts
+	// TODO TEMPORARY SECOND CONCEPT FOR COMPONENT TYPE CHECK - NEEDS TO BE REPLACEMENT
+	template <typename ComponentType>
+	concept IsValidComponentType2 = IsDerivedFrom<ComponentType, ECSComponent>;
 
 	class ECSKeyLockSystem final 
 	{
 	public:
-		ECSKeyLockSystem();
-		~ECSKeyLockSystem();
+		ECSKeyLockSystem() = default;
+		~ECSKeyLockSystem() = default;
 
 		void GenerateComponentKeys();
 
 		template <typename... TArgs>
-		ComponentBitList ConstructorBitList();
+		ComponentBitList ConstructComponentBitList() const;
 		
-		template <class T>
-		bool IsComponentBitTrue(ComponentBitList& a_bit_var) const;
+		template <typename TComponent>
+		requires IsValidComponentType2<TComponent>
+		bool IsComponentBitTrue(ComponentBitList const& a_bit_var) const;
 
-		template <class T>
-		void SetComponentBits(ComponentBitList& a_bit_var);
+		template <typename TComponent>
+		requires IsValidComponentType2<TComponent>
+		void SetComponentBits(ComponentBitList& a_bit_var) const;
 
-		template <class T>
-		void ResetComponentBits(ComponentBitList& a_bit_var);
+		template <typename TComponent>
+		requires IsValidComponentType2<TComponent>
+		void ResetComponentBits(ComponentBitList& a_bit_var) const;
 
-		template <class T>
+		template <typename TComponent>
+		requires IsValidComponentType2<TComponent>
 		int8 GetComponentBitPlacement() const;
 
+		template <typename TComponent>
+		requires IsValidComponentType2<TComponent>
+		void GetComponentBitPlacement(int8& a_component_bit_list) const;
+
 	private:
-		template <class T>
-		void InternalConstructorBitList(ComponentBitList& a_component_bit_list);
+		template <typename TComponent, typename... TArgs>
+		void InternalConstructComponentBitList(ComponentBitList& a_component_bit_list) const;
 
-		template <class TVoid, class TComponent, class... TArgs>
-		typename std::enable_if<std::is_base_of<ECSComponent, TComponent>::value && !is_same<ECSComponent, TComponent>::value, void>::type
-		InternalConstructorBitList(ComponentBitList& a_component_bit_list);
-
-		template <class TVoid, class TComponent, class... TArgs>
-		typename std::enable_if<!std::is_base_of<ECSComponent, TComponent>::value || is_same<ECSComponent, TComponent>::value, void>::type
-		InternalConstructorBitList(ComponentBitList& a_component_bit_list);
-
-		bool generated_component_keys_;
-
-		std::unordered_map<std::type_index, int8> component_bit_placement_;
+		static bool _generated_component_keys;
+		static std::unordered_map<std::type_index, int8> _component_bit_placement;
 
 	};
 
 #pragma region Template Function Implementation
 
 	template <typename... TArgs>
-	ComponentBitList ECSKeyLockSystem::ConstructorBitList()
+	ComponentBitList ECSKeyLockSystem::ConstructComponentBitList() const
 	{
 		ComponentBitList comp_bit_list(0);
-		InternalConstructorBitList<void, TArgs...>(comp_bit_list);
+		InternalConstructComponentBitList<TArgs...>(comp_bit_list);
 		return comp_bit_list;
 	}
 
-	template <class T>
-	void ECSKeyLockSystem::InternalConstructorBitList(ComponentBitList& /*a_component_bit_list*/)
+	template <typename TComponent>
+	requires IsValidComponentType2<TComponent>
+	bool ECSKeyLockSystem::IsComponentBitTrue(ComponentBitList const& a_bit_var) const 
 	{
-		return;
-	}
-
-	template <class TVoid, class TComponent, class... TArgs>
-	typename std::enable_if<std::is_base_of<ECSComponent, TComponent>::value && !is_same<ECSComponent, TComponent>::value, void>::type
-	ECSKeyLockSystem::InternalConstructorBitList(ComponentBitList& a_component_bit_list)
-	{
-		SetComponentBits<TComponent>(a_component_bit_list);
-		InternalConstructorBitList<TVoid, TArgs...>(a_component_bit_list);
-	}
-
-	template <class TVoid, class TComponent, class... TArgs>
-	typename std::enable_if<!std::is_base_of<ECSComponent, TComponent>::value || is_same<ECSComponent, TComponent>::value, void>::type
-	ECSKeyLockSystem::InternalConstructorBitList(ComponentBitList& a_component_bit_list)
-	{
-		static_assert(always_false<TComponent>::value, __FUNCTION__ " - Trying to construct bitilsit with a Component of type T that isn't derived from DECS::ECSComponent.");
-		return;
-	}
-
-	template <class T>
-	bool ECSKeyLockSystem::IsComponentBitTrue(ComponentBitList& a_bit_var) const 
-	{
-		auto& type	= typeid(T);
-		auto it		= component_bit_placement_.find(type);
-		if (it != component_bit_placement_.end()) 
+		int8 component_bit_placement(0);
+		GetComponentBitPlacement<TComponent>(component_bit_placement);
+		if (component_bit_placement != DFW_UNASSIGNED_COMPONENT_BIT) 
 		{
 			int64 temp (1);
-			temp = (temp << it->second);
-			// TODO - Compare bit, not final value.
+			temp = (temp << component_bit_placement);
 			return (a_bit_var & temp);
 		} 
-		else 
+		else
 		{
-			DFW_ERRORLOG("Unable to find ComponentBitPlacement of T: ", type.name());
 			return false;
 		}
 	}
 
-	template <class T>
-	void ECSKeyLockSystem::SetComponentBits(ComponentBitList& a_bit_var)
+	template <typename TComponent>
+	requires IsValidComponentType2<TComponent>
+	void ECSKeyLockSystem::SetComponentBits(ComponentBitList& a_bit_var) const
 	{
-		int64	temp (1);
-		int8	component_bit_placement = GetComponentBitPlacement<T>();
+		int64 temp (1);
+		int8 component_bit_placement(0);
+		GetComponentBitPlacement<TComponent>(component_bit_placement);
 		temp		= (temp << component_bit_placement);
 		a_bit_var	= (a_bit_var | temp);
 	}
 
-	template <class T>
-	void ECSKeyLockSystem::ResetComponentBits(ComponentBitList& a_bit_var) 
+	template <typename TComponent>
+	requires IsValidComponentType2<TComponent>
+	void ECSKeyLockSystem::ResetComponentBits(ComponentBitList& a_bit_var) const
 	{
-		int64	temp(1);
-		int8	component_bit_placement(GetComponentBitPlacement<T>());
+		int64 temp(1);
+		int8 component_bit_placement(0);
+		GetComponentBitPlacement<TComponent>(component_bit_placement);
 		temp		= ~(temp << component_bit_placement);
 		a_bit_var	=  (a_bit_var & temp);
 	}
 
-	template <class T>
+	template <typename TComponent>
+	requires IsValidComponentType2<TComponent>
 	int8 ECSKeyLockSystem::GetComponentBitPlacement() const 
 	{
-		auto& type	= typeid(T);
-		auto it		= component_bit_placement_.find(type);
-		if (it != component_bit_placement_.end())
+		int8 temp_bit_list(0);
+		GetComponentBitPlacement<TComponent>(temp_bit_list);
+		return temp_bit_list;
+	}
+
+	template <typename TComponent>
+	requires IsValidComponentType2<TComponent>
+	void ECSKeyLockSystem::GetComponentBitPlacement(int8& a_component_bit_list) const
+	{
+		const type_info& type = typeid(TComponent);
+		auto it = _component_bit_placement.find(type);
+		if (it != _component_bit_placement.end())
 		{
-			return it->second;
-		} 
-		else 
+			a_component_bit_list = it->second;
+		}
+		else
 		{
-			DFW_ERRORLOG("Unable to find ComponentBitPlacement of T: ", type.name());
-			return UNASSIGNED_COMPONENT_BIT;
+			DFW_ERRORLOG("Unable to find ComponentBitPlacement of TComponent: ", type.name());
+			a_component_bit_list = DFW_UNASSIGNED_COMPONENT_BIT;
+		}
+	}
+
+	template <typename TComponent, typename... TArgs>
+	void ECSKeyLockSystem::InternalConstructComponentBitList(ComponentBitList& a_component_bit_list) const
+	{
+		if constexpr (not IsValidComponentType2<TComponent>)
+		{
+			static_assert(always_false<TComponent>::value, __FUNCTION__ " - Trying to construct bitlist with a Component of type T that isn't derived from DECS::ECSComponent.");
+			return;
+		}
+		else if constexpr (sizeof...(TArgs) == 0)
+		{
+			SetComponentBits<TComponent>(a_component_bit_list);
+		}
+		else
+		{
+			SetComponentBits<TComponent>(a_component_bit_list);
+			InternalConstructorBitList<TArgs...>(a_component_bit_list);
 		}
 	}
 
