25185ef17c736636fab292534478b5be5fdfbaf8
TDRChan <thedigitalchan@gmail.com>
2022-06-24 14:30:14 +0200

    [Project, Core] Replacing typedefs with 'using' instead. #

--- Digital/Digital_/Source/Defines/Defines.h
+++ Digital/Digital_/Source/Defines/Defines.h
@@ -4,19 +4,17 @@
 #include <limits>
 #include <stdint.h>
 
-// POD Typedefs
-typedef float_t		float32;
-typedef double_t	float64;
-	
-typedef int8_t		int8;
-typedef int16_t		int16;
-typedef int32_t		int32;
-typedef int64_t		int64;
-	
-typedef uint8_t		uint8;
-typedef uint16_t	uint16;
-typedef uint32_t	uint32;
-typedef uint64_t	uint64;
+// POD Definitions.
+using float32	= float_t;
+using float64	= double_t;
+using int8		= int8_t;
+using int16		= int16_t;
+using int32		= int32_t;
+using int64		= int64_t;
+using uint8		= uint8_t;
+using uint16	= uint16_t;
+using uint32	= uint32_t;
+using uint64	= uint64_t;
 
 // Debug Assert Defines
 #if defined(DFW_PDEBUG64) || defined(DFW_DEBUG64) || defined(DFW_PRERELEASE64)
