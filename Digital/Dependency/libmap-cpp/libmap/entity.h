f2bb86400d808dc7ce5a51f7494d4e5e9368958f
TDRChan <thedigitalchan@gmail.com>
2023-05-03 15:28:20 +0200

    [Project, Dependency] Added 'libmap-cpp' for parsing/loading .map quake level files. #

---
+++ Digital/Dependency/libmap-cpp/libmap/entity.h
@@ -0,0 +1,41 @@
+#ifndef LIBMAP_ENTITY_H
+#define LIBMAP_ENTITY_H
+
+#include "vector.h"
+#include <stdlib.h>
+
+typedef struct LMBrush LMBrush;
+
+enum ENTITY_SPAWN_TYPE {
+	EST_WORLDSPAWN = 0,
+	EST_MERGE_WORLDSPAWN = 1,
+	EST_ENTITY = 2,
+	EST_GROUP = 3
+};
+
+typedef struct LMProperty {
+	char *key = NULL;
+	char *value = NULL;
+} LMProperty;
+
+class LMEntity {
+public:
+	int property_count = 0;
+	LMProperty *properties = NULL;
+
+	int brush_count = 0;
+	LMBrush *brushes = NULL;
+
+	vec3 center;
+	enum ENTITY_SPAWN_TYPE spawn_type;
+
+	bool has_property(const char* key);
+	const char* get_property(const char* key, const char* def = "");
+	int get_property_int(const char* key, int def = 0);
+	float get_property_float(const char* key, float def = 0.0f);
+	double get_property_double(const char* key, double def = 0.0);
+	vec2 get_property_vec2(const char* key, const vec2& def = { 0 });
+	vec3 get_property_vec3(const char* key, const vec3& def = { 0 });
+};
+
+#endif
