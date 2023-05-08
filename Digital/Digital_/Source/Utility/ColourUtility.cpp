5f6269a8dbeccbee66afd943bd2e45d4e1cf1e88
TDRChan <thedigitalchan@gmail.com>
2023-05-08 19:57:27 +0200

    [Project, Utility] Added simple ColourRGBA class to make colour stuff consistent across the codebase. #

---
+++ Digital/Digital_/Source/Utility/ColourUtility.cpp
@@ -0,0 +1,22 @@
+#include <Utility/ColourUtility.h>
+
+namespace DFW
+{
+    const ColourRGBA ColourRGBA::Black(0, 0, 0);
+    const ColourRGBA ColourRGBA::DarkRed(128, 0, 0);
+    const ColourRGBA ColourRGBA::Red(255, 0, 0);
+    const ColourRGBA ColourRGBA::DarkGreen(0, 128, 0);
+    const ColourRGBA ColourRGBA::Green(0, 255, 0);
+    const ColourRGBA ColourRGBA::DarkBlue(0, 0, 128);
+    const ColourRGBA ColourRGBA::Blue(0, 0, 255);
+    const ColourRGBA ColourRGBA::Yellow(255, 255, 0);
+    const ColourRGBA ColourRGBA::Purple(255, 0, 255);
+    const ColourRGBA ColourRGBA::Cyan(0, 255, 255);
+    const ColourRGBA ColourRGBA::Orange(255, 128, 0);
+    const ColourRGBA ColourRGBA::DarkOrange(128, 64, 0);
+    const ColourRGBA ColourRGBA::Grey(128, 128, 128);
+    const ColourRGBA ColourRGBA::DarkGrey(64, 64, 64);
+    const ColourRGBA ColourRGBA::LightGrey(192, 192, 192);
+    const ColourRGBA ColourRGBA::White(255, 255, 255);
+
+} // End of namespace ~ DFW.
