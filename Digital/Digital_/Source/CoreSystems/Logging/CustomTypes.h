a421efd620de3fc6f451d4f3c8c26b01907ad84c
TDRChan <thedigitalchan@gmail.com>
2023-08-07 17:45:16 +0200

    [Project, Logging] Adjusting log styling for glm vectors and quats. #

--- Digital/Digital_/Source/CoreSystems/Logging/CustomTypes.h
+++ Digital/Digital_/Source/CoreSystems/Logging/CustomTypes.h
@@ -40,8 +40,8 @@ template <> struct fmt::formatter<glm::vec2>
     format_context::iterator format(glm::vec2 const& a_vec2, format_context& a_ctx)
     {
         return presentation == 'f'
-            ? fmt::format_to(a_ctx.out(), "({:.3f}, {:.3f})", a_vec2.x, a_vec2.y)
-            : fmt::format_to(a_ctx.out(), "({:.3e}, {:.3e})", a_vec2.x, a_vec2.y);
+            ? fmt::format_to(a_ctx.out(), "[X={:.3f}, Y={:.3f}]", a_vec2.x, a_vec2.y)
+            : fmt::format_to(a_ctx.out(), "[X={:.3e}, Y={:.3e}]", a_vec2.x, a_vec2.y);
     }
 };
 
@@ -62,8 +62,8 @@ template <> struct fmt::formatter<glm::vec3>
     {
         // a_ctx.out() is an output iterator to write to.
         return presentation == 'f'
-            ? fmt::format_to(a_ctx.out(), "(X:{:.3f}, Y:{:.3f}, Z:{:.3f})", a_vec3.x, a_vec3.y, a_vec3.z)
-            : fmt::format_to(a_ctx.out(), "(X:{:.3e}, Y:{:.3e}, Z:{:.3e})", a_vec3.x, a_vec3.y, a_vec3.z);
+            ? fmt::format_to(a_ctx.out(), "[X={:.3f}, Y={:.3f}, Z={:.3f}]", a_vec3.x, a_vec3.y, a_vec3.z)
+            : fmt::format_to(a_ctx.out(), "[X={:.3e}, Y={:.3e}, Z={:.3e}]", a_vec3.x, a_vec3.y, a_vec3.z);
     }
 };
 
@@ -83,8 +83,8 @@ template <> struct fmt::formatter<glm::vec4>
     format_context::iterator format(glm::vec4 const& a_vec4, format_context& a_ctx) const 
     {
         return presentation == 'f'
-            ? fmt::format_to(a_ctx.out(), "({:.3f}, {:.3f}, {:.3f}, {:.3f})", a_vec4.x, a_vec4.y, a_vec4.z, a_vec4.w)
-            : fmt::format_to(a_ctx.out(), "({:.3e}, {:.3e}, {:.3e}, {:.3e})", a_vec4.x, a_vec4.y, a_vec4.z, a_vec4.w);
+            ? fmt::format_to(a_ctx.out(), "[X={:.3f}, Y={:.3f}, Z={:.3f}], W={:.3f}]", a_vec4.x, a_vec4.y, a_vec4.z, a_vec4.w)
+            : fmt::format_to(a_ctx.out(), "[X={:.3e}, Y={:.3e}, Z={:.3e}], W={:.3e}]", a_vec4.x, a_vec4.y, a_vec4.z, a_vec4.w);
     }
 };
 
@@ -104,8 +104,8 @@ template <> struct fmt::formatter<glm::quat>
     format_context::iterator format(glm::quat const& a_quat, format_context& a_ctx) const
     {
         return presentation == 'f'
-            ? fmt::format_to(a_ctx.out(), "({:.3f}, {:.3f}, {:.3f}, {:.3f})", a_quat.x, a_quat.y, a_quat.z, a_quat.w)
-            : fmt::format_to(a_ctx.out(), "({:.3e}, {:.3e}, {:.3e}, {:.3e})", a_quat.x, a_quat.y, a_quat.z, a_quat.w);
+            ? fmt::format_to(a_ctx.out(), "[X={:.3f}, Y={:.3f}, Z={:.3f}, W={:.3f}]", a_quat.x, a_quat.y, a_quat.z, a_quat.w)
+            : fmt::format_to(a_ctx.out(), "[X={:.3e}, Y={:.3e}, Z={:.3e}, W={:.3e}]", a_quat.x, a_quat.y, a_quat.z, a_quat.w);
     }
 };
 
