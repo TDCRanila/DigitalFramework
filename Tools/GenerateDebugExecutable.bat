49b410110d73a040c23963d4e79bda0f3a0e495d
TDRChan <thedigitalchan@gmail.com>
2024-10-03 14:31:41 +0200

    [Project, Tools] Adjusted clean-build script to latest changes (folder structure and project names). # - Added script for map resources that should be copied over to the build folder. - Changed use of VS19 to VS22.

--- Tools/GenerateDebugExecutable.bat
+++ Tools/GenerateDebugExecutable.bat
@@ -30,19 +30,19 @@ cd %basepath%
 cd ..
 mkdir "%projectpath%"
 cd %projectpath%
-cmake -G "Visual Studio 16 2019" %basepath% >>%logfilename%
+cmake -G "Visual Studio 17 2022" %basepath% >>%logfilename%
 
 :: Build Compile
 cd %projectpath%
 echo {} - MSVC - Compiling Project(s)
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-msbuild ProjectSolution.sln /t:_MainTargets\DigitalGame_ >>%logfilename% -m
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+msbuild ProjectSolution.sln /t:_MainTargets\DigitalGame >>%logfilename% -m
 echo {} - MSVC - Finished Compiling Project(s)
 
 :: Move Generated Objects
 echo {} - Copying Generated Objects.
 robocopy %projectpath% %execpath% %logfilename%
-robocopy %projectpath%\DigitalGame_\Debug %execpath%
+robocopy %projectpath%\ExampleProjects\DigitalGame\Debug %execpath%
 
 :: Copy Resources
 echo {} - Copying Resources ~ Shaders.
@@ -58,6 +58,14 @@ for /f %%d in ('dir  %basepath%\Resources\models /b /ad') do (
     robocopy  %basepath%\Resources\models\%%d %execpath%\models\%%d  *.* /s
     for /f %%e in ('dir  %basepath%\Resources\\models%%d /b /ad') do (
         robocopy  %basepath%\Resources\\models%%d\%%e %execpath%\\models\%%d\%%e *.* /s
+    )
+)
+
+echo {} - Copying Resources ~ Maps
+for /f %%d in ('dir  %basepath%\Resources\maps /b /ad') do (
+    robocopy  %basepath%\Resources\maps\%%d %execpath%\maps\%%d  *.* /s
+    for /f %%e in ('dir  %basepath%\Resources\\maps%%d /b /ad') do (
+        robocopy  %basepath%\Resources\\maps%%d\%%e %execpath%\\maps\%%d\%%e *.* /s
     )
 )
 
